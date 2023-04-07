2593072ba0383907740fb6396955e913ddf44f22
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-04-07 15:41:49 +0300

    Array_last_elements

---
+++ headFirstJS-17code.js
@@ -0,0 +1,4 @@
+var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+var last = products.length - 1;
+//Значение последнего элемента в массиве
+var recent = products[last];
