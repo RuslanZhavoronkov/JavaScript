fcdb0b369a3b0cb04392d60b21d4fc3d7fa9c555
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-04-17 17:03:35 +0300

    property_object_undefined

---
+++ headFirst-39-code.js
@@ -0,0 +1,10 @@
+//Проверяем свойство объекта
+var customer = {
+    name: "Jenny",
+    bar: "blue oyster"
+};
+
+if (customer.phoneNumber == undefined) {
+    console.log ("Get the client's phone number");
+}
+
