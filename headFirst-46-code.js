a315c4f872a5ad6ee9f2bb9eb7bf8e40a1676640
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-04-18 22:01:34 +0300

    experiment_numbers_string_boolens

---
+++ headFirst-46-code.js
@@ -0,0 +1,17 @@
+// сложение числа со строкой (операнды объединяются посредством конкатенации)
+console.log(3 + "4"); //34
+console.log("4" + 3); //43
+
+// умножение числа на строку
+console.log(3 * "4"); //12
+console.log(80/"10"); //8
+console.log("10" - 5); //5
+console.log(1 + 2 + "pizzas");//3pizzas
+console.log(1 + (2 + "pizzas"));//12pizzas (сначало выполняется действие в скобках)
+console.log(-true); //-1
+console.log(true + "love");//truelove
+console.log(3 + Number("4")); //7
+console.log(Infinity - 1); //Infinity
+console.log(2 + "1 1"); // 21 1
+console.log("1" - "1"); //0
+console.log(3 + " bananas " + 2 + " apples");
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-console.log(1 == "");
+console.log(3 + " bananas " + 2 + " apples");
