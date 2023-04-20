b7f757f213a5739064300f3c16c2cac20aa93a55
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-04-20 21:45:15 +0300

    test_telephone_number

--- headFirst-56-code.js
+++ headFirst-56-code.js
@@ -26,8 +26,8 @@ function validate(phoneNumber) {
     if (phoneNumber.length !== 8) {
         return false;
     }
-    var first = phoneNumber.substring(0,3);
-    var second = phoneNumber.substring(4);
+    var first = phoneNumber.substring(0,3); // создаем подстроку с 0 по 3 индекс
+    var second = phoneNumber.substring(4); // создаем подстроку с 4 по 7 индекс
 
     if (phoneNumber.charAt(3) !== "-" || isNaN(first) || isNaN(second)){
         return false;
@@ -35,3 +35,57 @@ function validate(phoneNumber) {
     return true;
 }
 validate("123-4567"); // true
+
+
+//Спецификация поменялась телефонный номер может содержать, а может и не содержать дефис
+
+//Комната Ларри
+function validate (phoneNumber) {
+    if (phoneNumber.length > 8 || phoneNumber.length < 7) {
+        return false;
+    }
+
+    for (var i = 0; i < phoneNumber.length; i++) {
+        if (i === 3) {
+            if (phoneNumber.length === 8 && phoneNumber.charAt(i) !== "-") {
+                return false;
+            } else if (phoneNumber.length === 7 && isNaN(phoneNumber.charAt(i))) {
+                return false;
+            }
+
+        } else if (isNaN(phoneNumber.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+validate(1234567); //true
+
+
+//Комната Брэда
+function validate (phoneNumber) {
+    if (phoneNumber.length > 8 || phoneNumber.length < 7) {
+        return false;
+    }
+
+    var first = phoneNumber.substring(0,3);
+    var second = phoneNumber.substring(phoneNumber.length - 4);
+
+    if (isNaN(first) || isNaN(second)) {
+        return false;
+    }
+
+    if (phoneNumber.length === 8) {
+        return (phoneNumber.charAt(3) === "-");
+    }
+    return true;
+}
+
+validate(1234567);
+
+//код Эмми
+function validate (phoneNumber) {
+    return phoneNumber.match(/^\d{3}-?\d{4}$/);
+}
+validate(1234567);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,18 +1,5 @@
+//код Эмми
 function validate (phoneNumber) {
-    if (phoneNumber.length !== 8) {
-        return false;
-    }
-
-    for (var i = 0; i < phoneNumber.length; i++) {
-        if (i === 3) {
-            if (phoneNumber.charAt(i) !== "-"){ //Сначала убеждаемся в том, что символ 3 содержит дефис
-                return false;
-            }
-        } else if (isNaN(phoneNumber.charAt(i))) { //А затем проверяем, что каждый символ с индексами 0-2 и 4-6 содержит цифру
-            return false;
-        }
-
-    }
-    return true;
+    return phoneNumber.match(/^\d{3}-?\d{4}$/);
 }
-validate("123-4567");
+validate(1234567);
