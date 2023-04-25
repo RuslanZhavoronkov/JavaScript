1f0bf35b144ff5ce6befe9ce325f21993f7056bd
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-04-25 22:28:48 +0300

    value1 + operation + value

---
+++ 22code_wars.js
@@ -0,0 +1,25 @@
+/*Ваша задача — создать функцию, которая выполняет четыре основные математические операции.
+
+Функция должна принимать три аргумента - операция(строка/символ), значение1 (число), значение2(число).
+Функция должна возвращать числовой результат после применения выбранной операции.*/
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+  return eval(value1+operation+value2);
+}
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
