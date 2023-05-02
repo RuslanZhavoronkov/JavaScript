c03cf3030d1049dbfba039abc3d5197b7f3a92aa
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-02 21:53:18 +0300

    Count of positives / sum of negatives

--- 38code_wars.js
+++ 38code_wars.js
@@ -1,29 +1,33 @@
 function countPositivesSumNegatives(input) {
-    // your code here
-    var sumkol = 0;
-    var multiple = 0;
-    var massive = [];
-    var massiveIn = input;
-    
-    
-    if (massiveIn.length === 0 || massiveIn === null) {
-        return massive;
-      }
-   
-    
-    for (var i = 0; i < massiveIn.length; i++) {
-      
-      if (massiveIn[i] > 0) {
-        sumkol++;
-            
-      } else if (massiveIn[i] < 0) {
-        multiple = multiple + massiveIn[i];
-      } 
-    }
-    
-    
-      massive.push(sumkol);
-      massive.push(multiple);
+  // your code here
+  var sumkol = 0;
+  var multiple = 0;
+  var massive = [];
+  var massiveIn = input;
+  
+   if (input === null) {
       return massive;
-   
   }
+  
+  if (massiveIn.length === 0) {
+      return massive;
+  }
+ 
+ 
+  
+  for (var i = 0; i < massiveIn.length; i++) {
+    
+    if (massiveIn[i] > 0) {
+      sumkol++;
+          
+    } else if (massiveIn[i] < 0) {
+      multiple = multiple + massiveIn[i];
+    } 
+  }
+  
+  
+    massive.push(sumkol);
+    massive.push(multiple);
+    return massive;
+ 
+}
