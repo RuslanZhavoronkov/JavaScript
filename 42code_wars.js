5499b252692545279a5fdadd17adc99e0802c5e8
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-04 19:12:21 +0300

    Math.min/max.apply(null,array)

---
+++ 42code_wars.js
@@ -0,0 +1,9 @@
+var min = function(list){
+    
+    return Math.min.apply(null, list); //Возвращает минимальное число массива
+}
+
+var max = function(list){
+    
+    return Math.max.apply(null, list); //Возвращает максимальное число массива
+}
