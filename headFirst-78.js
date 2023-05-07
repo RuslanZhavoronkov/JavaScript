f22bf3e16004430f8f04a9e6d8165830c339b7f3
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-07 15:54:13 +0300

    operational

---
+++ headFirst-78.js
@@ -0,0 +1,9 @@
+setTimeout (function() {
+    alert ("Time to take the cookies out of the oven");
+}, 600000);
+
+administer (patient, function (dosage) {
+    if (dosage > 0) {
+        inject (dosage);
+    }
+}, time);
