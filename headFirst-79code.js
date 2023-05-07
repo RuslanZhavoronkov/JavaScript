6a7810f3d5c625061f9b67bee42a8d7bb964c2e8
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-07 19:57:42 +0300

    global and local function

--- headFirst-79code.js
+++ headFirst-79code.js
@@ -1,18 +1,30 @@
 var migrating = true;
 
-if (migrating) {
-    quack(4);
-    fly(4);// ошибка, потому что функция fly еще не определена
-}
-
 var fly = function (num) {
+    var sound = "Flying";
+
+    function wingFlapper () { //Добавляется объявление функций с именем wingFlapper внутри функционального выражения fly
+        console.log(sound);
+    }
+
     for (var i = 0; i < num; i++) {
-        console.log ("Flying!");
+        wingFlapper(); //Здесь эта функция вызывается
     }
 };
 
 function quack(num) {
+    var sound = "Quack";
+
+    var quacker = function () { //Здесь добавляется функциональное выражение, присваемое переменной quacker внутри объявления функции quack
+        console.log (sound);
+    };
+
     for (var i = 0; i < num; i++) {
-        console.log ("Quack!");
+        quacker();// А здесь эта функция вызывается
     }
+}
+
+if (migrating) { //Этот код перемещен в конец, чтобы избежать ошибки при вызове fly
+    quack(4);
+    fly(4);
 }
