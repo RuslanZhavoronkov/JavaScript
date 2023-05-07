027713fd8cf9e434e327809e9ce54df8a8d7c984
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-07 20:22:37 +0300

    Is he gonna survive?

---
+++ 48code_wars.js
@@ -0,0 +1,12 @@
+function hero(bullets, dragons){
+    //Get Coding!
+      var gunsBullets = dragons * 2; 
+      
+      if (bullets >= gunsBullets) {
+        return true;
+      } else {
+        return false;
+      }
+      
+    }
+    
