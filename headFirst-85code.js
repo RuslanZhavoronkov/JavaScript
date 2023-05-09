19841c58b7be5a72c8f68cf65482846fb8949505
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-09 17:57:32 +0300

    return SetTimeout

--- headFirst-85code.js
+++ headFirst-85code.js
@@ -1,7 +1,7 @@
 function makeTimer (doneMessage, n) {
     setTimeout(function () {   //Метод setTimeout сохраняет функцию со свободным окружением (со свободной переменной doneMessage) 
         console.log (doneMessage);
-    }, 1000);
+    }, n);
 }
 
 makeTimer("Cookies are done", 1000); //происходит замыкание в методе setTimeout, потому что вместе с функцией передается окружение,
