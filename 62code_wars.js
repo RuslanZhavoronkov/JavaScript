036aa6ef797cc13ab3932c810d4c99ca2521ed96
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-17 22:31:33 +0300

    Grasshopper - Grade book

---
+++ 62code_wars.js
@@ -0,0 +1,15 @@
+function getGrade (s1, s2, s3) {
+    // Code here
+    var midNum = (s1 + s2 + s3)/3;
+    if (midNum >= 90 && midNum <= 100) {
+      return "A";
+    } else if (midNum >= 80 && midNum <= 90) {
+      return "B";
+    } else if (midNum >= 70 && midNum <= 80) {
+      return "C";
+    } else if (midNum >= 60 && midNum <= 70) {
+      return "D";
+    } else if (midNum >= 0 && midNum <= 60) {
+      return "F";
+    }
+  }
