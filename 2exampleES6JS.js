e744cda9f7ca325d13254b8929cf12ec57c4754f
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-25 06:52:44 +0300

    repeat let

--- 2exampleES6JS.js
+++ 2exampleES6JS.js
@@ -1,12 +1,16 @@
+let a = 0;
+let a = 1; //SyntaxError
+console.log(a);
+
 function myFunction () {
     let b = 2;
-    let b = 3;
+    let b = 3;//SyntaxError
 
     console.log(b);
 
     if(true) {
         let c = 4;
-        let c = 5;
+        let c = 5;//SyntaxError
     }
 
     console.log(c);
