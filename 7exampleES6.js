7e629dad67154ca2c5d1a97acdbd862b58d0fbe7
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-27 16:28:24 +0300

    ...arrays

--- 7exampleES6.js
+++ 7exampleES6.js
@@ -39,3 +39,14 @@ newArray2.push(... newArray1);
 console.log(newArray2);//[1, 2, 3, 4];
 
 //A.3 Расширение нескольких массивов
+let arraY1 = [1];
+let arraY2 = [2];
+let arraY3 = [... arraY1, ... arraY2, ... [3, 4]];
+let arraY4 = [5];
+
+function myFunction(a, b, c, d, e) {
+    return a + b + c + d + e;
+}
+
+let result = myFunction(... arraY3, ... arraY4);
+console.log(result); //15
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,11 @@
-let newArray1 = [2, 3, 4];
-let newArray2 = [1];
+let arraY1 = [1];
+let arraY2 = [2];
+let arraY3 = [... arraY1, ... arraY2, ... [3, 4]];
+let arraY4 = [5];
 
-newArray2.push(... newArray1);
-console.log(newArray2);
+function myFunction(a, b, c, d, e) {
+    return a + b + c + d + e;
+}
+
+let result = myFunction(... arraY3, ... arraY4);
+console.log(result);
