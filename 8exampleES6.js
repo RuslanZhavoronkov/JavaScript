f2a6e9fb67c3153c28874f06c847c38ce456eb5f
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-27 18:00:15 +0300

    Additional parameters function ES6

--- 8exampleES6.js
+++ 8exampleES6.js
@@ -1,3 +1,11 @@
+//Дополнительные параметры
+
+function myFunction(a, b, ...args) {
+    console.log(args);
+}
+
+myFunction(1, 2, 3, 4, 5); //[3, 4, 5] дополнительные аргументы
+
 let array1 = [1];
 let array2 = [2];
 let array3 = [...array1, ...array2, ... [3, 4]];
