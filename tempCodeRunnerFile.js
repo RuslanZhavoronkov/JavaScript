9b51ca436cddcbf96ca3f68a9e3721c860b420b3
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-30 12:58:21 +0300

    node

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,3 @@
-const sum = (...args) => {
-    //const args = [x, y, z];
-    let total = 0;
-    for (let i = 0; i < args.length; i++) {
-      total += args[i];
-    }
-    console.log(total);
-  }
-
-  sum(1, 2, 3, 4, 5);
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr);
+console.log(maximus);
