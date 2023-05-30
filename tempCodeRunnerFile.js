8dc4c414035d304069745ecf0171a0a84a0ce42b
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-05-30 16:55:47 +0300

    Destructuring via rest elements

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,7 @@
-let a = 8, b = 6;
-// Only change code below this line
-[a, b] = [b, a];
-console.log(a);
-console.log(b);
+function removeFirstTwo(list) {
+   const [ , ,...shorterList] = list; //это объект, переданный функции 
+    console.log (shorterList);
+  }
+  
+  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const sourceWithoutFirstTwo = removeFirstTwo(source);
