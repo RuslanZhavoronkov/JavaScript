4c4e8c415cb1d161e1606bbb118d7d02eaed28bc
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-03 12:28:04 +0300

    The Feast of Many Beasts

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,40 @@
-function sumMix(x){
-    let sum = 0;
-    for (let i = 0; i < x.length; i++) {
-      sum += Number(x[i]);
-    }
-    
-    console.log (sum);
-  }
+const sumArray = (array) => {
+    let result = 0;
+    let maxNum = 0;
+    let minNum = 0;
+    for (let i = 0; i < array.length; i++) {
+     if (array[i] === Math.max(...array)) {
+        maxNum += 1;
+        if (maxNum > 1) {
+            result += ((Math.max(...array) * maxNum) - Math.max(...array));
+        } else {
+            result += 0;
+        }
+      } 
+     if (array[i] === Math.min(...array)) {
+        minNum +=1;
+        if (minNum > 1) {
+            result += ((Math.min(...array) * mixNum) - Math.min(...array));
+        } else {
+            result += 0;
+        }
+     }
 
-  sumMix([9, 3, '7', '3']);
+     if (array === null || array === undefined || array === 0) {
+        result = 0;
+     } else {
+        result += array[i];
+     }
+      
+      
+       /* array[i] === Math.max(...array) ? maxNum +=1
+      : array[i] === Math.min(...array) ? minNum +=1
+      : maxNum > 1 ? result += maxNum
+      : minNum > 1 ? result += minNum
+      : array === 0 || array === [] || array === undefined ? result = 0 
+      : result += array[i]*/
+    }
+     console.log(result);
+   }
+   
+   sumArray([0, 1, 6, 10, 10]);
