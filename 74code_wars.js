cd5634cf5849dc1553532b8858a8d4c0b901bb43
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-03 15:12:11 +0300

    Parse nice int from char problem

---
+++ 74code_wars.js
@@ -0,0 +1,10 @@
+
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :)
+      let num = Number(inputString[0]);
+      return (typeof num) === 'number' ? Number(inputString[0]) : 0
+      }
+      
+    
+
+    getAge("4 years old");
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,40 +1,8 @@
-const sumArray = (array) => {
-    let result = 0;
-    let maxNum = 0;
-    let minNum = 0;
-    for (let i = 0; i < array.length; i++) {
-     if (array[i] === Math.max(...array)) {
-        maxNum += 1;
-        if (maxNum > 1) {
-            result += ((Math.max(...array) * maxNum) - Math.max(...array));
-        } else {
-            result += 0;
-        }
-      } 
-     if (array[i] === Math.min(...array)) {
-        minNum +=1;
-        if (minNum > 1) {
-            result += ((Math.min(...array) * mixNum) - Math.min(...array));
-        } else {
-            result += 0;
-        }
-     }
+function getAge(inputString){
+        // return the girl's correct age as an integer. Happy coding :) 
+          console.log( (typeof inputString[0]) === 'number' ? Number (inputString[0]) : 0);
+          }
+          
+    
 
-     if (array === null || array === undefined || array === 0) {
-        result = 0;
-     } else {
-        result += array[i];
-     }
-      
-      
-       /* array[i] === Math.max(...array) ? maxNum +=1
-      : array[i] === Math.min(...array) ? minNum +=1
-      : maxNum > 1 ? result += maxNum
-      : minNum > 1 ? result += minNum
-      : array === 0 || array === [] || array === undefined ? result = 0 
-      : result += array[i]*/
-    }
-     console.log(result);
-   }
-   
-   sumArray([0, 1, 6, 10, 10]);
+    getAge("4 years old");
