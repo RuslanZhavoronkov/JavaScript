53a7d6daa8c1e6c8e67fa14860d8fe05c3d0d107
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-18 14:54:43 +0300

    Exclusive or (xor) Logical Operator

---
+++ 117code_wars.js
@@ -0,0 +1,12 @@
+function xor(a, b) {
+  // TODO: Program Me
+  if (a == false && b == false) {
+    return false;
+  } else if (a == true && b == false) {
+    return true;
+  } else if (a == false && b == true) {
+    return true;
+  } else if (a == true && b == true) {
+    return false;
+  }
+}
