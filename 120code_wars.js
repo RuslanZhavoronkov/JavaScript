c1bdd109e01a69285daf734d9b6bdb6a932b9b9b
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-19 16:53:15 +0300

    Drink about

---
+++ 120code_wars.js
@@ -0,0 +1,9 @@
+function peopleWithAgeDrink(old) {
+  return old < 14
+    ? "drink toddy"
+    : old >= 14 && old < 18
+    ? "drink coke"
+    : old >= 18 && old < 21
+    ? "drink beer"
+    : "drink whisky";
+}
