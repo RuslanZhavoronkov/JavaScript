d921b265421a3bd287149089f72e22cb4f0ebfa6
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-07-21 09:42:39 +0300

    Hello, Name or World!

--- 122code_wars.js
+++ 122code_wars.js
@@ -1,4 +1,3 @@
-function addFive(num) {
-  let total = num + 5;
-  return total;
+function hello(name) {
+  return name ? `Hello, ${name[0].toUpperCase() + name.slice(1).toLowerCase()} !` : 'Hello, World!';
 }
